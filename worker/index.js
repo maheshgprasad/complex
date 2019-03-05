d7ee3a59060caac4e7d82808f4983246bfa04d6c
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-03-05 16:28:53 +0530

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,34 @@
+const keys = require('./keys');
+// keys for connection with the redis host.
+
+const redis = require('redis');
+
+// of-course a redis installation is required.
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000 //time in milliseconds
+});
+// connect to the redis client using this above strategy
+
+const sub = redisClient.duplicate();
+
+// subscription to user input
+
+function fib(index){
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
+// incase that the entered number is less than 2, 
+// return a value of 1 OR else 
+// perform a calculation.
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values',message, fib(parseInt(message)));
+});
+//anytime that we get a new value that shows up in redis
+// we are going to calculate a new fibonaaci value and then
+// insert that into a hash of values
+
+sub.subscribe('insert');
---
+++ worker/readme.md
@@ -0,0 +1,1 @@
+This is an attempt to set up multiple dependencies using various services to the web application
