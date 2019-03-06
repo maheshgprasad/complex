ba8ca3934ee5a09f370d809187d426b5233b9ba8
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-03-06 17:21:38 +0530

    commit changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
  
 
-//postgres client setup
+// PostGres client setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
     user: keys.pgUser,
@@ -39,3 +39,42 @@ const redisClient = redis.createClient({
     retry_strategy: () => 1000
 });
 const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get('/',(req, res) => {
+   res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+
+    res.send(values.rows);
+});
+
+app.get('/values/current',async(req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    });
+});
+
+app.post('/values', async (req, res) => {
+    const index = rew.body.index;
+    
+    if(parseInt(index) > 48) {
+        return res.status(422).send('INDEX TOO HIGH ');
+
+    }
+
+    redisClient.hset('values', index, 'Nothing Yet!');
+    redisPublisher.publish('insert',index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({ working: true});
+
+    });
+
+    app.listen(5000, err => {
+        console.log('Listening');
+    });
+    
