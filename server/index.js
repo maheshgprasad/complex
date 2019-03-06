27e122eca099769f37fdb35c09ad08d29cb52f6a
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-03-06 11:29:03 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -30,3 +30,12 @@ pgClient.on('error', ()=> console.log('PG CONNECTION LOST'));
 pgClient
 .query('CREATE TABLE IF NOT EXISTS values (number INT)')
 .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
