5491527614cbfdc9e9a111491df4f589cbc54baa
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-03-06 11:24:36 +0530

    server - index file added

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const keys = require ('./keys');
+// server access credentials
+
+// Express app setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+ 
+
+//postgres client setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+// log an error message when connection with PostGres is lost
+
+pgClient.on('error', ()=> console.log('PG CONNECTION LOST'));
+
+//create a table to store all the values
+
+pgClient
+.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+.catch(err => console.log(err));
