f4c727d1073d007ea4a88afb3ebf4ddaa75822e9
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-03-06 11:09:42 +0530

    keys.js file created and updated

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+//this file is going to house a method of communicating
+// with redis and postgres
+module.exports={
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+}
