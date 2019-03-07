913c20a70a5768344f77d3c52ed3d1fe250a86c3
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-03-07 10:42:39 +0530

    Bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import OtherPage from './otherpage';
-import Fib from './fib';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 class App extends Component {
   render() {
