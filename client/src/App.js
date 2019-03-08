84ac0a58d9e3b82e053497f2960f9536d283cb75
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-03-08 14:25:04 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-// import OtherPage from './OtherPage';
-import Fib from './Fib';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage.js';
+import Fib from './Fib.js';
 
 class App extends Component {
   render() {
@@ -11,14 +11,14 @@ class App extends Component {
       <Router>
         <div className="App">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
+              <img src={ logo } className="App-logo" alt="logo" />
               <h1 className="App-title">MGP</h1>
               <Link to="/">Home</Link>
-              {/* <Link to="/otherpage">Other Page</Link> */}
+              <Link to="/otherpage">Other Page</Link> 
             </header>
             <div>  
             <Route exact path="/" component={Fib} />
-            {/* <Route path="/otherpage" component={OtherPage}/> */}
+            <Route path="/otherpage" component={OtherPage}/> 
             </div>
         </div>
 
